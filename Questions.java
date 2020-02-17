199c5a7c4df0720abd8d05900b4c89b8e845507c
DanaMark <60191973+DanaMark@users.noreply.github.com>
2020-02-17 20:23:11 +0200

    Add files via upload

---
+++ Questions.java
@@ -0,0 +1,19 @@
+
+public class Questions {
+	
+
+		String prompt; 
+		String answer;
+		String explanation;
+		
+		
+		public Questions(String prompt, String answer, String explanation) {
+			
+			this.prompt = prompt;
+			this.answer = answer;
+			this.explanation = explanation;
+		} 
+		
+
+		
+	}
