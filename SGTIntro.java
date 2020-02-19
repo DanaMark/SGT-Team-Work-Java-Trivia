7f3d0e92f2acaf02fbe00e84cbbe6a6096e48a90
DanaMark <60191973+DanaMark@users.noreply.github.com>
2020-02-19 19:49:39 +0200

    Add files via upload

--- SGTIntro.java
+++ SGTIntro.java
@@ -37,12 +37,39 @@ public class SGTIntro {
 				+ " while some condition evaluates to true. Java provides three ways for executing the loops. \n"
 				+ "While all the ways provide similar basic functionality, they differ in their syntax and condition checking time. ";
 		
-		String exp8 = "";
+		String exp8 = "Char size is 16 bit i.e 2 byte. ";
 		
-		String exp9 = "";
+		String exp9 = "Using new keyword is the most basic way to create an object. This is the most common way to create an object in java. \n"
+					+ " Almost 99% of objects are created in this way. ";
 		
-		String exp10 = "";
+		String exp10 = "&& Conects two boolean expresions into one. Both expresions must be true for the overall expresion to be true";
 		
+		
+		
+		String q8 = "What is the size of a Char in Java? \n"
+				+ " A.  16 bits \n"
+			    + " B.  7 bits \n"
+			    + " C.  8 bits \n"
+			    + " D.  4 bits.";
+		
+		String q9 = "What is the keyword used in java to create an object? \n"
+				+ " A.  This \n"
+				+ " B.  Sync \n"
+			    + " C.  New \n"
+				+ " D.  New() ";
+		
+		String q10 = "If you want your condition to depend upon two conditions BOTH being true. \n" 
+				+ "What is the propper notation to put between the two Boolean statements?\n "
+				+ " A. 	&& \n"
+				+ " B.  ! \n"
+			    + " C.  == \n"
+				+ " D.  ||  ";
+		
+
+			   
+
+			    
+			    
 		
 		
 		
